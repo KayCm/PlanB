242f45ebe1f42f98a59deadff540156598d1e370
KayCM <rain@m1989.com>
2018-03-06 15:42:58 +0800

    Initial Commit

---
+++ PlanB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlanB
+//
+//  Created by KayCM on 2018/3/6.
+//  Copyright © 2018年 KayCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlanB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlanB
+//
+//  Created by KayCM on 2018/3/6.
+//  Copyright © 2018年 KayCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
